b48be287a9ceb893851ad747e694a567950ce9ab
ShyGamesStudio <shygamesstudio@gmail.com>
2023-12-15 10:07:37 +0100

    Removed some transaction

--- service/database/unban-user.go
+++ service/database/unban-user.go
@@ -3,7 +3,6 @@ package database
 import "github.com/SimoneDiCesare/WasaPhoto/service/database/queries"
 
 func (db *appdbimpl) UnbanUser(uid1 string, uid2 string) error {
-	db.c.Begin()
 	r, err := db.c.Exec(queries.UnbanUser, uid1, uid2)
 	if err != nil {
 		return err
